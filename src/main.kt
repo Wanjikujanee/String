a420d6a6dcba4d3abaac4a8796700a5acdb692dd
wanjikujanee <wanjikumaryjanee@gmail.com>
2022-03-02 08:17:23 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,42 @@
+fun main() {
+    var text = "Akirachix"
+    var A = text[0].toString() + text[2] + text[3]
+    println(A)
+
+
+    var x = "Jane"
+    var y = 20
+    var fullDetail = "Hi,my name is $x  and I am $y years old"
+    println(fullDetail)
+
+
+    var txt = "Nairobi"
+    println(txt.length)
+
+
+    var Name = "jane"
+    if (Name=="Kelly"){
+        println("that's me") }
+    else{
+        println("i dont know who that is")
+    }
+
+
+
+
+   var Name2 = "jane"
+    if(Name2=="jane"){
+println("that's me") }
+    else{
+    println("i dont know who that is")
+    }
+}
+
+
+
+
+
+
+
+
+
